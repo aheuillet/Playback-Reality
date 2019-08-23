182aebab2e44c7a5c1d6861a9aa9c0487d8e1823
Alexandre Heuillet <ale.heuillet@gmail.com>
2019-08-23 17:02:01 +0900

    added documentation and delete unused files

--- Assets/Custom/Scripts/TranslationGetter.cs
+++ Assets/Custom/Scripts/TranslationGetter.cs
@@ -44,31 +44,30 @@ class TransComp : IComparer<Model_Trans>
 
 public class TranslationGetter : MonoBehaviour
 {
-    private Settings settings;
+    //Default setting values
     private string SERVER_HOST = "192.168.4.77/middleware";
     private string DATABASE_NAME = "vicon";
     private string COLLECTION_NAME = "Mike_2019-08-14 04:25:17.710194";
     private string rootSegmentName;
     private List<Model_Trans> transList;
+    [SerializeField] Settings settings;
 
 
     // Start is called before the first frame update
     IEnumerator Start()
     {
-        settings = GameObject.Find("SettingsSingleton").GetComponent<Settings>();
-        if (settings) {
-            settings.LoadToScript();
-            DATABASE_NAME = settings.DatabaseName;
-            COLLECTION_NAME = settings.CollectionName;
-            SERVER_HOST = settings.ServerIP;
-        }        
+        settings.LoadToScript();
+        DATABASE_NAME = settings.DatabaseName;
+        COLLECTION_NAME = settings.CollectionName;
+        SERVER_HOST = settings.ServerIP;        
         string URL = "http://" + SERVER_HOST + "?collection=" + COLLECTION_NAME;
         IEnumerator couroutine = GetJsonTextFromURL(URL);
-        yield return StartCoroutine(couroutine);
+        yield return StartCoroutine(couroutine); // Wait for the coroutine to return
         transList.Sort(new TransComp());
-        this.gameObject.transform.Rotate(-79.15f, 0.0f, -173.728f); //Rotating GameObject to macth Unity axis orientation
+        this.gameObject.transform.Rotate(-86.955f, -62.139f, -111.994f); //Rotating GameObject to macth Unity axis orientation
     }
 
+    // This function send a WebRequest to obtain the motion data from the PHP middleware and deserialize it.
     IEnumerator GetJsonTextFromURL(string URL) {
         UnityWebRequest request = new UnityWebRequest();
         request = UnityWebRequest.Get(URL);
@@ -77,10 +76,11 @@ public class TranslationGetter : MonoBehaviour
             Debug.Log(request.error);
         }
         else {
-           transList = JsonConvert.DeserializeObject<List<Model_Trans>>(request.downloadHandler.text);
+           transList = JsonConvert.DeserializeObject<List<Model_Trans>>(request.downloadHandler.text); // Deserializing the JSON into a list of translations.
         }
     }
 
+    // Updating the position of every bone at every frame
     void LateUpdate()
     {
         if (transList != null) 
@@ -94,6 +94,7 @@ public class TranslationGetter : MonoBehaviour
         }
     }
 
+    // Format the given bone name to match those of the avatar's skeleton
     string strip(string BoneName)
     {
         if (BoneName.Contains(":"))
@@ -104,6 +105,7 @@ public class TranslationGetter : MonoBehaviour
         return BoneName;
     }
 
+    // Find the Root segment and apply the translations to all of its children.
     void FindAndTransform(Transform iTransform, string BoneName)
     {
         int ChildCount = iTransform.childCount;
@@ -121,6 +123,7 @@ public class TranslationGetter : MonoBehaviour
         }
     }
 
+    // Recursive function that browses the GameObject Tree to apply corresponding translations to every bone and its children.
     void TransformChildren(Transform iTransform)
     {
         int ChildCount = iTransform.childCount;
@@ -132,6 +135,7 @@ public class TranslationGetter : MonoBehaviour
         }
     }
 
+    // Returns the translation that matches the given bone name and delete it from the list (to prevent replaying frames).
     Model_Trans FindBoneInTransList(string BoneName)
     {
         foreach (Model_Trans trans in transList)
@@ -148,6 +152,7 @@ public class TranslationGetter : MonoBehaviour
         return a;
     }
 
+    // Apply the corresponding bone translation/rotation to a given Bone.
     private void ApplyBoneTransform(Transform Bone)
     {
         string BoneName = strip(Bone.gameObject.name);
@@ -160,6 +165,7 @@ public class TranslationGetter : MonoBehaviour
             Bone.localRotation = new Quaternion(Rot.x, -Rot.y, -Rot.z, Rot.w);
 
             Vector3 Translate = new Vector3((float)t.x_trans * 0.001f, (float)t.y_trans * 0.001f, (float)t.z_trans * 0.001f);
+            // flipping the x-axis
             Bone.localPosition = new Vector3(-Translate.x, Translate.y, Translate.z);
         }
     }
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-Playback Reality
+# Playback Reality
+
+A past explorer mixed reality app allowing you to visualize pre-recorded human motion data as holograms using a Microsoft Hololens headset.
+
+This app is powered by [Unity](https://unity.com) and built using [MRTK V2](https://microsoft.github.io/MixedRealityToolkit-Unity/README.html).
+
+⚠️⚠️ **Important:** This project uses a PHP middleware (REST API) to connect to a MongoDB database and download motion data. The PHP middleware can be found at [this repo](https://github.com/aheuillet/database-php-middleware).
+
+## Information
+
+This Unity app consists of two scenes:
+- `Menu` which is the main menu of the app, equipped with a *Start* button and a *Settings* button which allows you to tweak things (Server IP, database name...).
+- `Main` which is the main scene, containing a male avatar which is used to reproduced the movements using its attached *TranslationGetter.cs* script.
+
